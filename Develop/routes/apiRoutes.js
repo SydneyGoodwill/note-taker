242aa6cf8866ec2ac63998e163d0bc7dbe6fffb0
Sydney Goodwill <sydneyjogoodwill@gmail.com>
2020-10-06 17:44:41 -0600

    updated db.json and apiRoutes.js

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,11 +1,28 @@
-var noteData = require("../db/db.json");
+var fs = require("fs");
 
 module.exports = function(app) {
     app.get("/api/notes", function(req, res) {
-        res.json(noteData);
+        fs.readFile(__dirname + "/../db/db.json", "utf8", function(error, data) {
+            if (error) {
+                return console.log(error);
+              }
+            
+              console.log(data);
+              console.log(JSON.parse(data));
+              res.json(JSON.parse(data));
+        });
     });
 
     app.post("/api/notes", function(req, res) {
-        res.json(noteData);
+        fs.readFile(__dirname + "/../db/db.json", "utf8", function(error, data) {
+            var dataArray = JSON.parse(data);
+            dataArray.push(req.body)
+            fs.writeFile(__dirname + "/../db/db.json", JSON.stringify(dataArray), function(error) {
+                if (error) {
+                    return console.log(error);
+                }
+                res.json(dataArray);
+            })
+        })
     });
 }
