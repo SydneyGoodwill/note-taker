36d78abd29d2c2a7c4b678ad97193348148a60c1
Sydney Goodwill <sydneyjogoodwill@gmail.com>
2020-10-08 14:17:05 -0600

    updated api routes, DELETE still not fully functional

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -7,9 +7,8 @@ module.exports = function(app) {
                 return console.log(error);
               }
             
-              console.log(data);
-              console.log(JSON.parse(data));
               res.json(JSON.parse(data));
+              console.log(JSON.parse(data));
         });
     });
 
@@ -17,12 +16,28 @@ module.exports = function(app) {
         fs.readFile(__dirname + "/../db/db.json", "utf8", function(error, data) {
             var dataArray = JSON.parse(data);
             dataArray.push(req.body)
+            console.log(req.body);
             fs.writeFile(__dirname + "/../db/db.json", JSON.stringify(dataArray), function(error) {
                 if (error) {
                     return console.log(error);
                 }
                 res.json(dataArray);
             })
-        })
+        });
     });
+
+    
+
+    app.delete("/api/notes/:id", function(req, res) {
+        fs.readFile(__dirname + "/../db/db.json", "utf8", function(error, data) {
+            if (error) {
+                return console.log(error);
+            }
+
+            fs.unlink(__dirname + "/../db/db.json", function(error) {
+                if(error) return console.log(error);
+                console.log("File deleted successfully");
+            })
+        });
+    })
 }
